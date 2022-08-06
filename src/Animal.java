7b17f3154b757772818359e6d9076faaf5ca527e
Garrett <ga.landin89@gmail.com>
2022-08-06 15:02:15 -0500

    Initial Commit

---
+++ src/Animal.java
@@ -0,0 +1,15 @@
+public class Animal {
+    public static String iAmDog() {
+        return "I am a dog";
+    }
+
+    public static void doStuff() {
+
+        int a = 5;
+
+        while (a < 50) {
+            System.out.println("Hello!");
+            a+= 5;
+        }
+    }
+}
---
+++ src/LearnJava.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class LearnJava {
+    public static void main(String[] args) throws Exception {
+
+        // Animal a = new Animal();
+        // String dog = a.iAmDog();
+        // System.out.println(dog);
+        Animal a = new Animal();
+        a.doStuff();
+    }
+}
