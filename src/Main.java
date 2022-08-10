1dbb0bc916c458f4c2c50c4aa0a4ac2f5df2c30f
Garrett <ga.landin89@gmail.com>
2022-08-09 20:36:58 -0500

    Access modifiers and Encapsulation

--- src/Main.java
+++ src/Main.java
@@ -319,16 +319,31 @@ import java.util.Random;
 
 //? Abstraction *******************
 
+// public class Main {
+//     public static void main(String[] args) {
+
+//         // abstract = abstract classes cannot be instantiated, but they can have a subclass.
+//         // Abstract classes are declared without an implementation
+
+//         // Vehicle vehicle = new Vehicle();
+//         Car car = new Car();
+
+//         car.go();
+
+//     }
+// }
+
 public class Main {
     public static void main(String[] args) {
 
-        // abstract = abstract classes cannot be instantiated, but they can have a subclass.
-        // Abstract classes are declared without an implementation
+        // encapsulation = attributes of a class will be hidden or private, can be accessed only through methods (getters & setters)
+        // You should make attributes private if you dont have a reason to make them public or protected
 
-        // Vehicle vehicle = new Vehicle();
-        Car car = new Car();
-
-        car.go();
+        Car car = new Car("Ford", "Mustang", 2021);
+        car.setYear(2000);
+        System.out.println(car.getMake());
+        System.out.println(car.getModel());
+        System.out.println(car.getYear());
 
     }
 }
