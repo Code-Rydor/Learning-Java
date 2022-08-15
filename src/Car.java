aff5c1aaff20cf7317de5d668463ab883890dddc
Garrett <ga.landin89@gmail.com>
2022-08-15 17:03:54 -0500

    Learning how to copy objects and about interfaces

--- src/Car.java
+++ src/Car.java
@@ -89,7 +89,7 @@
 //     }
 // }
 
-// Polymorphism //
+//? Polymorphism //
 // public class Car extends Vehicle {
 
 //     @Override
@@ -99,12 +99,55 @@
 // }
 
 //? Pass objects as arguments to methods //
-public class Car {
+// public class Car {
 
-    String name;
+//     String name;
 
-    Car(String name) {
-        this.name = name;
-    }
+//     Car(String name) {
+//         this.name = name;
+//     }
 
-}
+// }
+
+//? Copying objects //
+// public class Car {
+//     private String make;
+//     private String model;
+//     private int year;
+
+//     Car(String make, String model, int year) {
+//         this.setMake(make);
+//         this.setModel(model);
+//         this.setYear(year);
+//     }
+
+//     public String getMake() {
+//         return make;
+//     }
+
+//     public String getModel() {
+//         return model;
+//     }
+
+//     public int getYear() {
+//         return year;
+//     }
+
+//     public void setMake(String make) {
+//         this.make = make;
+//     }
+
+//     public void setModel(String model) {
+//         this.model = model;
+//     }
+
+//     public void setYear(int year) {
+//         this.year = year;
+//     }
+
+//     public void copy(Car x) {
+//         this.setMake(x.getMake());
+//         this.setModel(x.getModel());
+//         this.setYear(x.getYear());
+//     }
+// }
--- src/Garage.java
+++ src/Garage.java
@@ -1,11 +1,11 @@
 //? Pass objects as arguments to methods //
-public class Garage {
+// public class Garage {
 
-    void park(Car car) {
-        System.out.println("The " + car.name + " is parked in the garage!");
-    }
-    void park(Animal animal) {
-        System.out.println("The " + animal.name + " is parked in the garage!");
-    }
+//     void park(Car car) {
+//         System.out.println("The " + car.name + " is parked in the garage!");
+//     }
+//     void park(Animal animal) {
+//         System.out.println("The " + animal.name + " is parked in the garage!");
+//     }
     
-}
+// }
